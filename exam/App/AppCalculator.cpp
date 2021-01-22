b86633e53e5554414d0be6e55ffd9768ccf8d950
dbakalina <darya.bakalina@gmail.com>
2021-01-22 09:34:13 +0300

    Exam

--- exam/App/AppCalculator.cpp
+++ exam/App/AppCalculator.cpp
@@ -6,11 +6,12 @@ using namespace System;
 using namespace System::Windows::Forms;
 
 [STAThread]
-void main(array<String^>^ arg) {
+int main(array<String^>^ arg) {
     Application::EnableVisualStyles();
     Application::SetCompatibleTextRenderingDefault(false);
     App::AppCalculator form;
     Application::Run(% form);
 
-    //return 0;
+    return 0;
 }
+
